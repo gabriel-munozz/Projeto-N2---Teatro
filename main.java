cb14f3aca11f73f3ed72b7fb08549f41f10bb722
Gabriel Machado Palacios Muñoz <gabrielmachadomunoz@gmail.com>
2025-11-06 11:07:32 +0000

    menu

--- main.java
+++ main.java
@@ -6,12 +6,15 @@ public class main{
 
         int opcao = 0;
 
-        System.out.println("***MACK THEATHER***");
-        System.out.println("1.Cadastrar Espetáculo");
-        System.out.println("2.Cadastrar Cliente");
-        System.out.println("3.Compra de Entradas");
-        System.out.println("4.Sair");
-        System.out.print("Selecione uma opção: ");
-        opcao = entrada.nextInt();
+        do{
+            System.out.println("***MACK THEATHER***");
+            System.out.println("1.Cadastrar Espetáculo");
+            System.out.println("2.Cadastrar Cliente");
+            System.out.println("3.Compra de Entradas");
+            System.out.println("4.Sair");
+            System.out.print("Selecione uma opção: ");
+            opcao = entrada.nextInt();
+        } while (opcao != 4);
+
     }
 }
